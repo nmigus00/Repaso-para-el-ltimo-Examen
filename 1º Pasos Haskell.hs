61601a761ee92881dd325e564285f52ff0594431
nmigus00 <nmigus00@estudiantes.unileon.es>
2023-06-06 00:29:20 +0200

    Repasando para el examen

---
+++ 1º Pasos Haskell.hs
@@ -0,0 +1,53 @@
+{-¿Qué es un paradigma de prgramación?
+Modelo conceptual que abarca un conjunto de técnicas y principios 
+de diseño para definir un determinado estilo de programación.-}
+
+{-¿Qué es un Lenguaje de Programación?
+Es la forma de expresar un programa mediante carácterísticas 
+que reflejen los conceptos esenciales de uno o varios paradigmas.-}
+
+{-Principales paradigmas de programación:
+    1º Programación imperativa
+    2º Programación declarativa
+    
+Dentro de la Programación Declarativa se encuentra la Programación Funcional-}
+
+{-Características de la Programación Funcional:
+    1º El programados describe cuál debe ser el resultado del proceso
+    2º Los Programas se basan en dos apesctos:
+        - Datos representados como valores
+        - Computación representada como la evaluación de funciones-}
+
+{-Haskell es puro.
+Las expresiones son siempre referencialmente transparentes:
+    1º Todo es inmutable
+    2º No existen los efectos colaterales
+    3º Su evaluación es determinista (Los datos de entrada darán la misma salida)-}
+
+{-Haskell es perezoso.
+Las expresiones no se evalúan hasta que sus resultados no sean necesarios-}
+
+{-Haskell es estáticamente tipado.
+Toda expresión tiene un tipo de datos concreto
+    - Posee un sistema de inferencia de tipos
+Todos los tipos son comprobados en tiempo de compilación
+    - Evita errores de tipo antes de la ejecución del programa-}
+
+
+--Ejercicios
+
+--Ejercicio 1
+sum' :: [Int] -> Int
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+--Ejercicio 2
+product' :: [Int] -> Int
+product' [] = 1
+product' (x:xs) = x * product' xs
+
+--Ejercicio 3
+qsort' [] = []
+qsort' (x:xs) = qsort' larger ++ [x] ++ qsort' smaller
+              where smaller = [a | a <- xs, a <= x]
+                    larger = [b | b <- xs, b > x]
---
+++ Programación en Haskell.hs
@@ -0,0 +1,48 @@
+{-¿Qué es un compilador?
+Es un programa que traduce código escrito en un lenguaje 
+de programación en otro lenguaje objetivo-}
+
+{-¿Qué es un Intérprete?
+Programa que ejecuta instrucciones espécificas escritas 
+en el código del lenguaje fuente junto a una entrada 
+suministrada por el usuario de forma directa-}
+
+--Ejercicios
+--Ejercicio 1
+--Haskell evaluaría la expresión como f(a) + b
+
+{-Ejercicio 2
+((2 ^ 3)* 4) ------- ((2 * 3) + (4 * 5)) ------- (2 + (3 * (4 ^ 5)))-}
+
+{-Ejercicio 3
+> 4 * (7 + 2) - 3 * 9
+Prelude> 4 * (7 + 2) - 3 * 9
+9
+> 18 `div` 9 `mod` 2
+Prelude> 18 `div` 9 `mod` 2
+0
+> 4 * 9 > 3 + 3
+Prelude> 4 * 9 > 3 + 3
+True-}
+
+{-Ejercicio 4
+> (2 - `sqrt` (4 + 32)) / 2 {-Notación infija o en línea-}
+Prelude> (2 - sqrt (4 + 32)) / 2
+-2.0
+
+> (/) ((+) (- sqrt ((+) 4 32)) (2)) 2 {-Notación prefija o polaca-}
+Prelude> (/) ((+) (- sqrt ((+) 4 32)) (2)) 2 
+-2.0-}
+
+{-Ejercicio 5
+1º Las funciones no pueden empezar por letra mayúscula
+2º Las comillas del div no son las correctas para que funcione la función
+3º No hay una buena identación en la parte después del where xs tendría que estar a la altura de la variable a-}
+
+--Ejercicio 6
+last' [x] = x
+last' (_:xs) = last' [xs]
+
+--Ejercicio 7
+init' [x] = []
+init' (x:xs) = x : init' xs
